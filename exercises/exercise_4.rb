d23b7c3db2d084dfeca00aae81219be9461d1dde
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-26 02:05:01 -0400

    Complete Exercise 5 - Calculations

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,13 +11,13 @@ Whistler_store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_ap
 Yaletown_store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 @mens_store = Store.where("mens_apparel = ?", true).each { |store| 
-  puts "Store Name: #{store.name}, Annual Revenu: #{store.annual_revenue}"
+  puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
 }
 
 # can also do: Store.where(mens_apparel: true)
 
 @womens_store = Store.where("womens_apparel = ? AND annual_revenue < 1000000", true).each { |store| 
-  puts "Store Name: #{store.name}, Annual Revenu: #{store.annual_revenue}"
+  puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
 }
 
 # can also do: Store.where(womens_apparel: true, annual_revenue: ..1000000)
