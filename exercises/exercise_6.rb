6ef217fc6f48774fe5047612f3042866e4b0d05d
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-26 02:12:08 -0400

    Complete Exercise 6 - One-to-many-association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Calvin", last_name: "Ho", hourly_rate: 100)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 50)
+@store2.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "Sally", last_name: "Lo", hourly_rate: 100)
+@store2.employees.create(first_name: "Bob", last_name: "Poe", hourly_rate: 50)
